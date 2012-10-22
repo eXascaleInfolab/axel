2a90881e00d4aadac7b486ed84ff50c212196c29
Roman Prokofyev <roman.prokofyev@gmail.com>
2012-10-22 14:51:44 +0200

    update article admin

--- axel/articles/admin.py
+++ axel/articles/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from axel.articles.models import Article, ArticleAuthor, Author, Venue
 
 
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('venue', 'year', 'link', 'citations', 'pdf')
+    list_display = ('title', 'venue', 'year')
+
 admin.site.register(Venue)
 admin.site.register(ArticleAuthor)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Author)
