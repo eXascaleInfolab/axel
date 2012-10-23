0959e9a6b4750a77dfacc125fd03b9fdb927dcc8
Roman Prokofyev <roman.prokofyev@gmail.com>
2012-10-24 02:07:58 +0300

    added view to show collocations

--- axel/articles/views.py
+++ axel/articles/views.py
@@ -1,1 +1,17 @@
-__author__ = 'dragoon'
+from django.views.generic.edit import FormView
+
+from axel.articles.forms import PDFUploadForm
+
+
+class PDFCollocationsView(FormView):
+    """Extract and display collocations from pdf document"""
+    template_name = 'articles/pdfcollocations.html'
+    form_class = PDFUploadForm
+
+    def form_valid(self, form):
+        """
+        Do valid form post-processing, extract collocations
+        :type form: PDFUploadForm
+        """
+        # TODO: render same page with collocations included in the context
+        return super(PDFCollocationsView, self).form_valid(form)
