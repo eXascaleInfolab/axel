4aa86094612ed9aa0af828581e95583389fc25b4
Roman Prokofyev <roman.prokofyev@gmail.com>
2012-10-24 10:59:00 +0200

    add haystack search urls

--- axel/urls.py
+++ axel/urls.py
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    (r'^search/', include('haystack.urls')),
 )
