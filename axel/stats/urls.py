12a8070d52c272a08149d702ae6cfd3be340ea9e
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-02-09 14:44:29 +0100

    added initial ngram participation graph

--- axel/stats/urls.py
+++ axel/stats/urls.py
@@ -1,5 +1,6 @@
 """Url mappings"""
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from axel.stats.views import CollocationStats, ConceptIndexStats, FilteredCollectionModelView, CollocationMainView
 
 
@@ -7,5 +8,8 @@ urlpatterns = patterns('axel.stats.views',
     url(r'^$', CollocationMainView.as_view(), name='stats'),
     url(r'^(?P<model_name>[^/]+)/$', CollocationStats.as_view(), name='count_dist'),
     url(r'^(?P<model_name>[^/]+)/ci/$', ConceptIndexStats.as_view(), name='ci_stats'),
+    url(r'^(?P<model_name>[^/]+)/ngram_participation/$',
+        TemplateView.as_view(template_name='stats/graph_vis/ngram_particiation.html'),
+        name='ngram_participation_stats'),
     url(r'^(?P<model_name>[^/]+)/filter/$', FilteredCollectionModelView.as_view(), name='colloc_filter')
 )
