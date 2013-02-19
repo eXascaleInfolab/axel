14c1d853d7e32cfb4fca07055ebaf05aa771129a
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-02-19 17:47:47 +0100

    fixing ACM search

--- axel/stats/scores/dl_acm_search.py
+++ axel/stats/scores/dl_acm_search.py
@@ -1,4 +1,5 @@
 from lxml import html
+import urllib
 import requests
 import re
 
@@ -9,10 +10,8 @@ COUNT_RE = re.compile(r'Results 1 - 20 of ([\d,]+)')
 def acm_search_result_count(ngram):
     """Get search result count from ACM Digital Library"""
     headers = {'User-Agent': "Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1"}
-    lxml_content = html.fromstring(requests.get('http://dl.acm.org', headers=headers).content)
+    #lxml_content = html.fromstring(requests.get('http://dl.acm.org', headers=headers).content)
     # extract search form action
-    search_form_url = lxml_content.forms[0].action
-    payload = {'query': '"{0}"'.format(ngram)}
-    result = requests.post('http://dl.acm.org/{0}'.format(search_form_url),
-        data=payload, headers=headers).content
-    return int(COUNT_RE.findall(result)[0])
+    result = requests.get('http://dl.acm.org/results.cfm?query=%22{0}%22'.format(urllib.quote_plus(ngram)),
+        headers=headers).content
+    return int(COUNT_RE.findall(result)[0].replace(',',''))
