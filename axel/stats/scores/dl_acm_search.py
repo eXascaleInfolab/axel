441281a12babb14fdd599e905e925db06845998b
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-02-25 15:23:17 +0100

    add utf-8 encoding before quotation

--- axel/stats/scores/dl_acm_search.py
+++ axel/stats/scores/dl_acm_search.py
@@ -12,6 +12,7 @@ def acm_search_result_count(ngram):
     headers = {'User-Agent': "Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1"}
     #lxml_content = html.fromstring(requests.get('http://dl.acm.org', headers=headers).content)
     # extract search form action
-    result = requests.get('http://dl.acm.org/results.cfm?query=%22{0}%22'.format(urllib.quote_plus(ngram)),
+    ngram = urllib.quote_plus(ngram.encode('utf-8'))
+    result = requests.get('http://dl.acm.org/results.cfm?query=%22{0}%22'.format(ngram),
         headers=headers).content
     return int(COUNT_RE.findall(result)[0].replace(',',''))
