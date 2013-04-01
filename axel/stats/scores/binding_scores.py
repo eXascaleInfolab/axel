666858f1d0a11b935b43dbed2d996280414d9c07
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-04-01 13:32:13 +0200

    added new function to compute binding scores for many grams

--- axel/stats/scores/binding_scores.py
+++ axel/stats/scores/binding_scores.py
@@ -7,7 +7,7 @@ from axel.articles.utils.nlp import build_ngram_index
 NGRAM_REGEX = ur'(?:\w|-)'
 
 
-def weight_both_ngram(ngram, text, article_dict):
+def weight_both_ngram(ngram, text):
     """AVERAGE BETWEEN TWO SCORES"""
     score = 0
     space_index = -1
@@ -31,7 +31,7 @@ def weight_both_ngram(ngram, text, article_dict):
     return score / denominator
 
 
-def weight_both_ngram1(ngram, text, article_dict):
+def weight_both_ngram1(ngram, text):
     """AVERAGE BETWEEN TWO WEIGHTED SCORES"""
     score = 0
     space_index = -1
@@ -54,7 +54,7 @@ def weight_both_ngram1(ngram, text, article_dict):
     return score / denominator
 
 
-def weight_both_ngram2(ngram, text, article_dict):
+def weight_both_ngram2(ngram, text):
     """MODIFIER DETERMINER"""
     score = 0
     space_index = -1
@@ -74,17 +74,45 @@ def weight_both_ngram2(ngram, text, article_dict):
         score2 = distribution_dict[w2]
         score = (score1 + score2) / (N1 + N2)
         # 4% increase in MAP
-        if N2_len / N1_len >= 5:
-            score /= 2
-        elif N1_len / N2_len >= 5:
-            score += 1
+        #if N2_len / N1_len >= 5:
+        #if N1_len / N2_len >= 5:
+        #    score += 1
+        score /= N2_len/N1_len
         denominator += 1
     return score / denominator
 
 
-def weight_ngram_score(ngram, text, article_dict, ngram_abs_count):
+def weight_both_ngram3(ngram, text, split_ngram=None):
+    """MODIFIER DETERMINER + SUBTRACT EXISTING"""
+    score = 0
+    space_index = -1
+    denominator = 0
+    while True:
+        space_index = ngram.find(' ', space_index + 1)
+        if space_index == -1:
+            break
+        w1, w2 = ngram[:space_index], ngram[space_index + 1:]
+        if split_ngram and w1 != split_ngram and not w2 != split_ngram:
+            continue
+        # subtract existing prefixes/suffixes from distribution dicts
+        distribution_dict = Counter(re.findall(ur'({1}+) {0}'.format(w2, NGRAM_REGEX), text, re.U))
+        N2 = sum(distribution_dict.values())
+        N2_len = len(distribution_dict)
+        score1 = distribution_dict[w1]
+        distribution_dict = Counter(re.findall(ur'{0} ({1}+)'.format(w1, NGRAM_REGEX), text, re.U))
+        N1 = sum(distribution_dict.values())
+        N1_len = len(distribution_dict)
+        score2 = distribution_dict[w2]
+        score = (score1 + score2) / (N1 + N2)
+        # 4% increase in MAP
+        score /= N2_len/N1_len
+        denominator += 1
+    return score / denominator
+
+
+def weight_ngram_score(ngram, text, article_dict, ngram_abs_count, score_func=weight_both_ngram3):
     if len(ngram.split()) == 2:
-        return weight_both_ngram2(ngram, text, article_dict)
+        return score_func(ngram, text)
     else:
         smaller_ngrams = set(build_ngram_index(ngram).keys()).intersection(article_dict.keys())
         # select max split combination
@@ -96,16 +124,28 @@ def weight_ngram_score(ngram, text, article_dict, ngram_abs_count):
                 # reduce score from the consumed ngram according its score
                 article_dict[smaller_ngram] = (smaller_ngram_count,
                                                smaller_ngram_score - score, is_rel)
+                score = (score + score_func(ngram, text, smaller_ngram)) / 2
             else:
+                score = 0
                 smaller_ngrams = sorted(smaller_ngrams, key=lambda x: len(x.split()), reverse=True)
-                smaller_ngram_count, smaller_ngram_score, is_rel = article_dict[smaller_ngrams[0]]
-                score = smaller_ngram_score * ngram_abs_count / smaller_ngram_count
-                article_dict[smaller_ngrams[0]] = (smaller_ngram_count,
-                                                   smaller_ngram_score - score, is_rel)
+                for smaller_ngram in smaller_ngrams:
+                    smaller_ngram_count, smaller_ngram_score, is_rel = article_dict[smaller_ngram]
+                    local_score = smaller_ngram_score * ngram_abs_count / smaller_ngram_count
+                    article_dict[smaller_ngram] = (smaller_ngram_count,
+                                                   smaller_ngram_score - local_score, is_rel)
+                    score += local_score
         # no - full average
         else:
-            score = weight_both_ngram2(ngram, text, article_dict)
+            score = score_func(ngram, text)
         return score
+
+
+class ArticleNgramBindings(object):
+
+    def __init__(self, article):
+        self.article_dict = {}
+        self.article = article
+        self.stemmed_text = article.stemmed_text
 
 
 def populate_article_dict(queryset, score_func):
