7a77ce8fe36b54bd3392720d1e30d814991e47fa
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-04-02 21:12:12 +0200

    added dictionary display on click

--- axel/stats/scores/binding_scores.py
+++ axel/stats/scores/binding_scores.py
@@ -7,8 +7,8 @@ from axel.articles.utils.nlp import build_ngram_index
 NGRAM_REGEX = ur'(?:\w|-)'
 
 
-def weight_ngram_score(ngram, text, article_dict, ngram_abs_count, corr_dict=None, score_func='weight_both_ngram4'):
-    nb = NgramBindings(ngram, text, corr_dict=corr_dict)
+def weight_ngram_score(ngram, text, article_dict, ngram_abs_count, corr_dict1=None, corr_dict2=None, score_func='weight_both_ngram4'):
+    nb = NgramBindings(ngram, text, corr_dict1=corr_dict1, corr_dict2=corr_dict2)
     if len(ngram.split()) == 2:
         score = getattr(nb, score_func)()
     else:
@@ -33,16 +33,17 @@ def weight_ngram_score(ngram, text, article_dict, ngram_abs_count, corr_dict=Non
         # no - full average
         else:
             score = getattr(nb, score_func)()
-    return score
+    return score, nb.ddict1, nb.ddict2
 
 
 class NgramBindings(object):
 
-    def __init__(self, ngram, stemmed_text, corr_dict=None):
+    def __init__(self, ngram, stemmed_text, corr_dict1=None, corr_dict2=None):
         self.values_dict = {}
         self.text = stemmed_text
         self.ngram = ngram
-        self.corr_dict = corr_dict
+        self.corr_dict1 = corr_dict1 or {}
+        self.corr_dict2 = corr_dict2 or {}
 
     def weight_both_ngram(self, split_ngram=None):
         """AVERAGE BETWEEN TWO SCORES"""
@@ -156,7 +157,6 @@ class NgramBindings(object):
         score = 0
         space_index = -1
         denominator = 0
-        corr_dict = self.corr_dict or {}
         while True:
             space_index = ngram.find(' ', space_index + 1)
             if space_index == -1:
@@ -166,9 +166,9 @@ class NgramBindings(object):
                 continue
             # subtract existing prefixes/suffixes from distribution dicts
             regex = u'(' + (ur'{0}+ '.format(NGRAM_REGEX) * len(w1.split()))[:-1] + u') ' + w2
-            distribution_dict = Counter(re.findall(regex, self.text, re.U))
-            if w2 in corr_dict:
-                for w in corr_dict[w2]:
+            self.ddict1 = distribution_dict = Counter(re.findall(regex, self.text, re.U))
+            if w2 in self.corr_dict1:
+                for w in self.corr_dict1[w2]:
                     if w != w1:
                         distribution_dict.pop(w, None)
             N2 = sum(distribution_dict.values())
@@ -176,9 +176,9 @@ class NgramBindings(object):
             score1 = distribution_dict[w1]
 
             regex = w1 + u' (' + (ur' {0}+'.format(NGRAM_REGEX) * len(w2.split()))[1:] + u')'
-            distribution_dict = Counter(re.findall(regex, self.text, re.U))
-            if w1 in corr_dict:
-                for w in corr_dict[w1]:
+            self.ddict2 = distribution_dict = Counter(re.findall(regex, self.text, re.U))
+            if w1 in self.corr_dict2:
+                for w in self.corr_dict2[w1]:
                     if w != w2:
                         distribution_dict.pop(w, None)
             N1 = sum(distribution_dict.values())
@@ -201,12 +201,13 @@ def populate_article_dict(queryset, score_func):
     for article in Article.objects.filter(cluster_id=queryset.model.CLUSTER_ID):
         text = article.stemmed_text
         # create correspondence dict
-        corr_dict = defaultdict(set)
+        corr_dict1 = defaultdict(set)
+        corr_dict2 = defaultdict(set)
         for ngram in article.articlecollocation_set.values_list('ngram', flat=True):
             if len(ngram.split()) == 2:
                 w1, w2 = ngram.split()
-                corr_dict[w2].add(w1)
-                corr_dict[w1].add(w2)
+                corr_dict1[w2].add(w1)
+                corr_dict2[w1].add(w2)
         for ngram in sorted(article.articlecollocation_set.all(),
                             key=lambda x: len(x.ngram.split())):
             if ngram.ngram in rel_ngram_set:
@@ -218,7 +219,9 @@ def populate_article_dict(queryset, score_func):
             ngram_abs_count = text.count(ngram.ngram)
             if ngram_abs_count <= 5:
                 continue
-            score = score_func(ngram.ngram, text, article_dict[article], ngram_abs_count, corr_dict)
+            score, ddict1, ddict2 = score_func(ngram.ngram, text, article_dict[article],
+                                               ngram_abs_count, corr_dict1, corr_dict2)
             article_dict[article][ngram.ngram] = {'abs_count': ngram_abs_count, 'score': score,
-                                                  'is_rel': is_rel, 'count': ngram.count}
+                                                  'is_rel': is_rel, 'count': ngram.count,
+                                                  'ddict1': ddict1, 'ddict2': ddict2}
     return article_dict
