6d663c4e239164663d618cbb02f60157d7e449f6
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-06-13 18:11:34 +0200

    multiple fixed for edits extraction

--- axel/ngrams/management/commands/analyze_edits.py
+++ axel/ngrams/management/commands/analyze_edits.py
@@ -8,6 +8,9 @@ import difflib
 from axel.ngrams.models import Sentence, Edit
 
 SENTENCE_REGEX = re.compile(r'[.!?]')
+FILTER_REGEX = re.compile(r'[^\w\s\d]')
+LINE_END_REPLACE_REGEX = re.compile(r'(?P<end>[.?!])(\\r\\n)+')
+MULTIPLE_PUNCT_REGEX = re.compile(r'([.!?])+')
 
 
 class Command(BaseCommand):
@@ -17,20 +20,26 @@ class Command(BaseCommand):
 
     def _get_sentence(self, text, sentences, index1):
         for i, index in enumerate(sentences):
+            if i + 1 == len(sentences):
+                return text[sentences[i]:]
             if index1 > index:
                 continue
             if i == 0:
                 return text[:index].strip()
             else:
                 return text[sentences[i-1]+1:index].strip()
+        return text
 
     def handle(self, *args, **options):
-        insertCounter = {}
-        deleteCounter = {}
+        totalCounter = []
 
         for edit_file in args:
             for line in open(edit_file).read().split('\n'):
-                line = line.strip().replace('\\r\\n', '\n')
+                # treat line breaks as sentences
+                line = line.strip()
+                line = LINE_END_REPLACE_REGEX.sub('\g<end>', line)
+                line = line.replace('\\r\\n', '')
+                line = MULTIPLE_PUNCT_REGEX.sub('\g<1>', line)
                 if not line or line == "null":
                     continue
                 for edit in line.split('|||'):
@@ -43,9 +52,14 @@ class Command(BaseCommand):
                                 continue
                             str1, str2 = edit1[i1:i2], edit2[j1:j2]
 
-                            # skip edits that contain line breaks and sentence seprators
+                            # skip edits that contain line breaks and sentence separators
                             if set('.!?\n') & set(str1 + str2):
                                 continue
+
+                            # skip bad edits:
+                            if FILTER_REGEX.search(str1) or FILTER_REGEX.search(str2):
+                                continue
+
                             sentence1 = self._get_sentence(edit1, sentences1, i1)
                             sentence2 = self._get_sentence(edit2, sentences2, j1)
 
@@ -54,14 +68,12 @@ class Command(BaseCommand):
                                 #insertCounter.append(str2)
                                 #deleteCounter.append(str1)
                             elif tag == 'delete':
-                                deleteCounter[str1] = (sentence1, sentence2, Edit.DELETE)
+                                totalCounter.append((str1, sentence1, sentence2, Edit.DELETE))
                             elif tag == 'insert':
-                                insertCounter[str2] = (sentence1, sentence2, Edit.INSERT)
-        insertKeys = [x for x in Counter(insertCounter.keys()).iteritems() if x[1] > 1]
-        deleteKeys = [x for x in Counter(deleteCounter.keys()).iteritems() if x[1] > 1]
-        totalKeys = insertKeys + deleteKeys
+                                totalCounter.append((str2, sentence1, sentence2, Edit.INSERT))
+        totalKeys = set([x[0] for x in Counter(zip(*totalCounter)[0]).iteritems() if x[1] > 1])
 
-        for key in totalKeys:
-            sentence1, sentence2, edit_type = insertCounter[key]
-            sen = Sentence.objects.create(sentence1=sentence1, sentence2=sentence2)
-            Edit.objects.create(sentence=sen, edit_type=edit_type, edit1=key)
+        for edit, sentence1, sentence2, edit_type in totalCounter:
+            if edit in totalKeys:
+                sen = Sentence.objects.create(sentence1=sentence1, sentence2=sentence2)
+                Edit.objects.create(sentence=sen, edit_type=edit_type, edit1=edit)
