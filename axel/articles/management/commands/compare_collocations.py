9d8cbe13fd5bad235ca7caa4aa1e234a6649c57e
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-09-17 12:55:37 +0200

    added precision/recall comparison to comparison command

--- axel/articles/management/commands/compare_collocations.py
+++ axel/articles/management/commands/compare_collocations.py
@@ -1,4 +1,6 @@
 """Compares two different collocation candidate sets"""
+from __future__ import division
+from collections import defaultdict
 from optparse import make_option
 from termcolor import colored
 
@@ -28,10 +30,40 @@ class Command(BaseCommand):
 
         model = CLUSTERS_DICT[cluster]
 
+        self.article_rel_dict = defaultdict(lambda: {0: set(), 1: set()})
+        for key, is_rel in model.judged_data.iteritems():
+            ngram, article_id = key.split(',')
+            is_rel = int(is_rel)
+            self.article_rel_dict[article_id][is_rel].add(ngram)
+
+        # precision-recall
+        old_prec = []
+        old_rec = []
+        new_prec = []
+        new_rec = []
+
         for article in Article.objects.filter(cluster_id=cluster):
             print article, article.pdf
             test_collocs = list(TestCollocations.objects.filter(article=article).values_list('ngram', 'count'))
             cur_collocs = list(model.objects.filter(article=article).values_list('ngram', 'count'))
+
+            # START: calculate precision-recall
+            correct_objects = self.article_rel_dict[unicode(article)][1]
+            incorrect_objects = self.article_rel_dict[unicode(article)][0]
+            true_pos_old = [x for x in zip(*cur_collocs)[0] if x in correct_objects]
+            true_pos_new = [x for x in zip(*test_collocs)[0] if x in correct_objects]
+            false_pos_old = [x for x in zip(*cur_collocs)[0] if x in incorrect_objects]
+            false_pos_new = [x for x in zip(*test_collocs)[0] if x in incorrect_objects]
+            local_old_prec = len(true_pos_old) / (len(true_pos_old) + len(false_pos_old))
+            local_new_prec = len(true_pos_new) / (len(true_pos_new) + len(false_pos_new))
+            old_prec.append(local_old_prec)
+            new_prec.append(local_new_prec)
+            old_rec_local = len(true_pos_old) / len(correct_objects)
+            new_rec_local = len(true_pos_new) / len(correct_objects)
+            old_rec.append(old_rec_local)
+            new_rec.append(new_rec_local)
+            # END: calculate precision-recall
+
             obsolete_collocs = set(zip(*cur_collocs)[0]).difference(zip(*test_collocs)[0])
             print colored('Obsolete collocations:', 'red')
             print colored(obsolete_collocs, 'red')
@@ -48,4 +80,14 @@ class Command(BaseCommand):
                         obj.save()
                         print obj.pos_tag, obj.pos_tag_prev, obj.pos_tag_after
             print
+
+        precision_old = sum(old_prec) / len(old_prec)
+        precision_new = sum(new_prec) / len(new_prec)
+        recall_old = sum(old_rec) / len(old_rec)
+        recall_new = sum(new_rec) / len(new_rec)
+
+        print 'Current Precision: ', precision_old
+        print 'New Precision: ', precision_new
+        print 'Current Recall: ', recall_old
+        print 'New Recall: ', recall_new
 
