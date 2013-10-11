a5e84822ce5f62a93e925cb55d82d0060f1cdd52
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-10-11 14:00:01 +0200

    moved n-gram to another project

--- axel/ngrams/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from axel.ngrams.views import SentenceList, SentenceDetailView
-
-
-urlpatterns = patterns('axel.ngrams.views',
-    url(r'^$', SentenceList.as_view(), name='sentences'),
-    url(r'^(?P<pk>\d+)/$', SentenceDetailView.as_view(), name='sentence_detail'),
-)
