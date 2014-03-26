8a993667086d408f5bade8cd3cad2990beaaa93a
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-03-26 15:14:55 +0100

    keep only pdf collocations

--- axel/urls.py
+++ axel/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from axel.articles.views import ConceptualSearchView
+from axel.articles.views import ConceptualSearchView, PDFCollocationsView
 
 admin.autodiscover()
 
@@ -13,8 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    (r'^articles/', include('axel.articles.urls')),
+    #(r'^articles/', include('axel.articles.urls')),
     #(r'^stats/', include('axel.stats.urls')),
     #(r'^testc/', include('test_collection.urls')),
     #url(r'^$', ConceptualSearchView.as_view(), name='main')
+    url(r'^$', PDFCollocationsView.as_view(), name='main'),
 )
